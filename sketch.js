3cb6620f97973b29ac3595342f768417de7fa297
sayali-redasani <74544224+sayali-redasani@users.noreply.github.com>
2022-05-21 13:01:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400,400);
+
+  var ball = createSprite(200, 200, 10 , 10)
+}
+
+function draw() 
+{
+  background(30);
+
+  drawSprites();
+
+
+}
+
+
+//workspace
+
